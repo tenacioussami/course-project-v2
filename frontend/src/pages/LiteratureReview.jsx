aaa28e45babde861c94b8be5e8fd44cf835ebae1
tenacioussami <mdfarhanhossainsami@gmail.com>
2026-09-24 23:12:48 +0600

    Switch to react-quill-new to fix editor breaking on type

--- frontend/src/pages/LiteratureReview.jsx
+++ frontend/src/pages/LiteratureReview.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Plus, Pencil, Trash2, Search, ChevronDown, ChevronUp } from 'lucide-react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 import api from '../services/api';
 import Loading from '../components/Loading';
 import Modal from '../components/Modal';
